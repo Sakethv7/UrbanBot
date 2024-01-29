eb158158344a4a9c0bcd44903675ba10cbccb1fc
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-01-29 01:13:08 -0700

    Add files via upload

---
+++ iBot.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+namespace MyDiscordBot
+{
+    public interface IBot
+    {
+        Task StartAsync(ServiceProvider services);
+
+        Task StopAsync();
+    }
+}
